45728936ccb85cb75f0ed25f7871d7e201f17904
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-08-08 05:00:20 +0900

    MoviePage Create Loadmore button

--- client/src/components/MoviePage/MoviePage.js
+++ client/src/components/MoviePage/MoviePage.js
@@ -4,41 +4,58 @@ import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../config/config";
 import GridCards from "../commons/GridCards";
 
 function MoviePage() {
-  const [MovieList, setMovieList] = useState([]);
   const [Movie, setMovie] = useState([]);
+  const [Page, setPage] = useState(0);
 
   useEffect(() => {
     const moiveData = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`;
-    axios.get(moiveData).then((res) => {
-      console.log("Movie data22222 : ", res.data.results);
-      setMovie(res.data.results);
-    });
+
+    fetchMovies(moiveData);
   }, []);
 
-  const renderGridCard = Movie.map((movie, index) => (
-    <React.Fragment key={index}>
-      <GridCards
-        image={
-          movie.poster_path ? `${IMAGE_BASE_URL}w200${movie.poster_path}` : null
-        }
-        movieId={movie.id}
-        movieName={movie.title}
-      />
-    </React.Fragment>
-  ));
+  const fetchMovies = (moiveData) => {
+    axios.get(moiveData).then((res) => {
+      console.log("Movie data22222 : ", res.data.results);
+      setMovie((Movie) => [...Movie, res.data.results]);
+      setPage(res.data.page);
+    });
+  };
+  console.log("Movie : ", Movie);
+
+  const loadMoreMovies = () => {
+    const moiveData = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR&page=${
+      Page + 1
+    }`;
+
+    fetchMovies(moiveData);
+  };
+  console.log("Page : ", Page);
+
+  const renderGridCard = Movie.map((movies, index) =>
+    movies.map((movie, index) => (
+      <React.Fragment key={index}>
+        <GridCards
+          image={
+            movie.poster_path
+              ? `${IMAGE_BASE_URL}w200${movie.poster_path}`
+              : null
+          }
+          movieId={movie.id}
+          movieName={movie.title}
+        />
+      </React.Fragment>
+    ))
+  );
 
   //   console.log("Movie data : ", Movie);
 
   return (
     <div>
       <div className="row">{Movie && renderGridCard}</div>
-      <div className="text-center justify-content-between">
-        <a href="">1</a>
-        <a href="">2</a>
-        <a href="">3</a>
-        <a href="">4</a>
-        <a href="">5</a>
+      <div className="text-center">
+        <button onClick={loadMoreMovies}>Load More</button>
       </div>
+      {/* <div className="text-center justify-content-between"></div> */}
     </div>
   );
 }
--- client/src/components/commons/GridCards.js
+++ client/src/components/commons/GridCards.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function GridCards(props) {
+  console.log("props : ", props);
   return (
     <div className="col">
       <div>
