e76bf360ddac569999d784d8f0b4715d2a26e5c0
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-08-25 04:14:42 +0900

    Update Server search & homePost & erveryPost

--- server/router/post/search/search.js
+++ server/router/post/search/search.js
@@ -27,7 +27,7 @@ router.post("/", (req, res) => {
   if (Category === "전체") {
     console.log(1);
     let query = connection.query(
-      "select postId, title, description, date, view, categoryName, name, email, role from post left join category on post.category = category.categoryNumber left join user on post.writer = user.id where name = ? OR title = ? order by postId desc",
+      "select postId, title, description, date, view, categoryName, name, email, role from post left join category on post.category = category.categoryNumber left join user on post.writer = user.id where name = ? OR title = ? order by date desc",
       [Search, Search],
       (err, result) => {
         if (err) return res.send(err);
@@ -43,7 +43,7 @@ router.post("/", (req, res) => {
   } else if (Category === "제목") {
     console.log(2);
     let query = connection.query(
-      "select postId, title, description, date, view, categoryName, name, email, role from post left join category on post.category = category.categoryNumber left join user on post.writer = user.id where title = ? order by postId desc",
+      "select postId, title, description, date, view, categoryName, name, email, role from post left join category on post.category = category.categoryNumber left join user on post.writer = user.id where title = ? order by date desc",
       [Search],
       (err, result) => {
         if (err) return res.send(err);
@@ -59,7 +59,7 @@ router.post("/", (req, res) => {
   } else {
     console.log(3);
     let query = connection.query(
-      "select postId, title, description, date, view, categoryName, name, email, role from post left join category on post.category = category.categoryNumber left join user on post.writer = user.id where name = ? order by postId desc",
+      "select postId, title, description, date, view, categoryName, name, email, role from post left join category on post.category = category.categoryNumber left join user on post.writer = user.id where name = ? order by date desc",
       [Search],
       (err, result) => {
         if (err) return res.send(err);
