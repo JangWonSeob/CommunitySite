fca060f36359df4a74693aee5b1be73c7434cf3c
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-08-25 03:35:30 +0900

    Update Server auth

--- server/router/user/auth/auth.js
+++ server/router/user/auth/auth.js
@@ -4,18 +4,16 @@ const router = express.Router();
 router.get("/", (req, res) => {
   if (req.session.passport) {
     let session = req.session.passport.user;
-    if (!session) {
-      return res.json({ isAuth: false });
-    } else {
-      return res.status(200).json({
-        id: session.id,
-        isAdmin: session.role === "normal" ? false : true,
-        isAuth: true,
-        email: session.email,
-        name: session.name,
-        role: session.role,
-      });
-    }
+    return res.status(200).json({
+      id: session.id,
+      isAdmin: session.role === "normal" ? false : true,
+      isAuth: true,
+      email: session.email,
+      name: session.name,
+      role: session.role,
+    });
+  } else {
+    return res.json({ isAuth: false });
   }
 });
 
